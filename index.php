95468971fb893d034cdf903bdc06cb7b9b168210
fazlulrachmatmubbaraq <85746526+fazlulrachmatmubbaraq@users.noreply.github.com>
2023-02-26 23:19:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,405 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+
+    <title><?php echo "Personal Web"; ?></title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="css/magnific-popup.css" rel="stylesheet">
+
+    <link href="css/templatemo-first-portfolio-style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <section class="preloader">
+        <div class="spinner">
+            <span class="spinner-rotate"></span>
+        </div>
+    </section>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+
+            <a href="index.php" class="navbar-brand mx-auto mx-lg-0"><?php echo "Blog Pribadi"; ?></a>
+
+
+            <div class="d-flex align-items-center d-lg-none">
+                <i class="navbar-icon bi-telephone-plus me-3"></i>
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-lg-5">
+                    <li class="nav-item">
+                        <a class="nav-link click-scroll" href="#section_1">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link click-scroll" href="#section_2">About</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link click-scroll" href="#section_3">Services</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link click-scroll" href="#section_4">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+    </nav>
+
+    <main>
+
+        <section class="hero d-flex justify-content-center align-items-center" id="section_1">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-7 col-12">
+                        <div class="hero-text">
+                            <div class="hero-title-wrap d-flex align-items-center mb-4">
+                                <img src="images/foto profile.jpg" class="avatar-image avatar-image-large img-fluid" alt="">
+
+                                <h1 class="hero-title ms-3 mb-0">Hello!</h1>
+                            </div>
+
+                            <h2 class="mb-4"><?php echo "Saya Fazlul Rachmat Mubbaraq"; ?></h2>
+                            <p class="mb-4"><a class="custom-btn btn custom-link" href="#section_2">Mulai</a></p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5 col-12 position-relative">
+                        <div class="hero-image-wrap"></div>
+                        <img src="images/portrait-happy-excited-man-holding-laptop-computer.png" class="hero-image img-fluid" alt="">
+                    </div>
+
+                </div>
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#535da1" fill-opacity="1" d="M0,160L24,160C48,160,96,160,144,138.7C192,117,240,75,288,64C336,53,384,75,432,106.7C480,139,528,181,576,208C624,235,672,245,720,240C768,235,816,213,864,186.7C912,160,960,128,1008,133.3C1056,139,1104,181,1152,202.7C1200,224,1248,224,1296,197.3C1344,171,1392,117,1416,90.7L1440,64L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"></path>
+            </svg>
+        </section>
+
+
+        <section class="about section-padding" id="section_2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-12">
+                        <img src="images/couple-working-from-home-together-sofa.jpg" class="about-image img-fluid" alt="">
+                    </div>
+
+                    <div class="col-lg-6 col-12 mt-5 mt-lg-0">
+                        <div class="about-thumb">
+
+                            <div class="section-title-wrap d-flex justify-content-end align-items-center mb-4">
+                                <h2 class="text-white me-4 mb-0"><?php echo "Cerita Saya"; ?></h2>
+
+                                <img src="images/foto profile.jpg" class="avatar-image img-fluid" alt="">
+                            </div>
+
+                            <h3 class="pt-2 mb-3"><?php echo "Cerita singkat tentang diri saya"; ?></h3>
+
+                            <p>Nama saya Fazlul Rachmat Mubbaraq, Saya mahasiswa Program Studi Ilmu Komputer FMIPA UHO. Asal saya dari Kota Raha, Kabupaten Muna.</p>
+                            <h3 class="rit-titl"><i class="fas fa-briefcase"></i> Pengalaman Organisasi</h3>
+                            <div class="work-exp">
+                                <h6>Osis SMAN 2 Raha <span>(2018-2019)</span></h6>
+                                <i>Anggota Bidang 4 Kepribadian dan Budi Pekerti Luhur</i>
+                            </div>
+                            <div class="work-exp">
+                                <h6>Pramuka Gudep Arjuna-Srikandi <span>(2019-2020)</span></h6>
+                                <i>Ketua Divisi Kegiatan dan Operasional</i>
+                            </div>
+                            <div class="work-exp">
+                                <h6>HMPS Ilmu Komputer <span>(2022-2023)</span></h6>
+                                <i>Sekretaris Divisi Hubungan Masyarakat</i>
+                            </div>
+                            <div class="work-exp">
+                                <h6>HMPS Ilmu Komputer <span>(2023-Sekarang)</span></h6>
+                                <i>Koordinator Divisi Hubungan Masyarakat</i>
+                            </div>
+                            <h3 class="rit-titl"><i class="fas fa-graduation-cap"></i> Riwayat Pendidikan</h3>
+                            <div class="education">
+                                <ul class="row no-margin">
+                                    <li class="col-md-6"><span>2008-2014</span> <br>
+                                        SDN 15 Katobu (Sekarang SDN 9 Katobu)</li>
+                                    <li class="col-md-6"><span>2014-2017</span> <br>
+                                        SMPN 1 Raha</li>
+                                    <li class="col-md-6"><span>2017-2020</span> <br>
+                                        SMAN 2 Raha</li>
+                                    <li class="col-md-6"><span>2020-Sekarang</span> <br>
+                                        S1 Ilmu Komputer - Universitas Haluoleo</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="featured section-padding">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-12">
+                        <div class="profile-thumb">
+                            <div class="profile-title">
+                                <h4 class="mb-0"><?php echo "Informasi Kontak"; ?></h4>
+                            </div>
+
+                            <div class="profile-body">
+                                <p>
+                                    <span class="profile-small-title">Name</span>
+                                    <span>Fazlul Rachmat Mubbaraq.</span>
+                                </p>
+
+                                <p>
+                                    <span class="profile-small-title">Birthday</span>
+                                    <span>21 Februari 2002.</span>
+                                </p>
+
+                                <p>
+                                    <span class="profile-small-title">Phone</span>
+                                    <span><a href="tel:082296539096.">082296539096.</a></span>
+                                </p>
+
+                                <p>
+                                    <span class="profile-small-title">Email</span>
+                                    <span><a href="mailto:fazlulrachmat18@gmail.com.">fazlulrachmat18@gamail.com.</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </section>
+
+
+
+        <section class="services section-padding" id="section_3">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-10 col-12 mx-auto">
+                        <div class="section-title-wrap d-flex justify-content-center align-items-center mb-5">
+                            <img src="images/handshake-man-woman-after-signing-business-contract-closeup.jpg" class="avatar-image img-fluid" alt="">
+
+                            <h2 class="text-white ms-4 mb-0">Services</h2>
+                        </div>
+
+                        <div class="row pt-lg-5">
+                            <div class="col-lg-6 col-12">
+                                <div class="services-thumb">
+                                    <div class="d-flex flex-wrap align-items-center border-bottom mb-4 pb-3">
+                                        <h3 class="mb-0">Websites</h3>
+
+                                        <div class="services-price-wrap ms-auto">
+                                            <p class="services-price-text mb-0">$2,400</p>
+                                            <div class="services-price-overlay"></div>
+                                        </div>
+                                    </div>
+
+                                    <p>You may want to explore Too CSS for great collection of free HTML CSS templates.</p>
+
+                                    <a href="#" class="custom-btn custom-border-btn btn mt-3">Discover More</a>
+
+                                    <div class="services-icon-wrap d-flex justify-content-center align-items-center">
+                                        <i class="services-icon bi-globe"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-12">
+                                <div class="services-thumb">
+                                    <div class="d-flex flex-wrap align-items-center border-bottom mb-4 pb-3">
+                                        <h3 class="mb-0">Ecommerce</h3>
+
+                                        <div class="services-price-wrap ms-auto">
+                                            <p class="services-price-text mb-0">$3,600</p>
+                                            <div class="services-price-overlay"></div>
+                                        </div>
+                                    </div>
+
+                                    <p>If you need a customized ecommerce website for your business, feel free to discuss with me.</p>
+
+                                    <a href="#" class="custom-btn custom-border-btn btn mt-3">Discover More</a>
+
+                                    <div class="services-icon-wrap d-flex justify-content-center align-items-center">
+                                        <i class="services-icon bi-phone"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-12">
+                                <div class="services-thumb">
+                                    <div class="d-flex flex-wrap align-items-center border-bottom mb-4 pb-3">
+                                        <h3 class="mb-0">Android App</h3>
+
+                                        <div class="services-price-wrap ms-auto">
+                                            <p class="services-price-text mb-0">$3,600</p>
+                                            <div class="services-price-overlay"></div>
+                                        </div>
+                                    </div>
+
+                                    <p>If you need a customized Android for your business, feel free to discuss with me.</p>
+
+                                    <a href="#" class="custom-btn custom-border-btn btn mt-3">Discover More</a>
+
+                                    <div class="services-icon-wrap d-flex justify-content-center align-items-center">
+                                        <i class="services-icon bi-phone"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+        </section>
+
+
+
+        <section class="contact section-padding" id="section_5">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-md-10 col-14">
+                        <div class="section-title-wrap d-flex justify-content-center align-items-center mb-5">
+                            <img src="images/aerial-view-man-using-computer-laptop-wooden-table.jpg" class="avatar-image img-fluid" alt="">
+
+                            <h2 class="text-white ms-4 mb-0">Say Hi</h2>
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="col-lg-3 col-md-6 col-12 pe-lg-0">
+                        <div class="contact-info contact-info-border-start d-flex flex-column">
+                            <strong class="site-footer-title d-block mb-3">Services</strong>
+
+                            <ul class="footer-menu">
+                                <li class="footer-menu-item"><a href="#" class="footer-menu-link">Websites</a></li>
+
+                                <li class="footer-menu-item"><a href="#" class="footer-menu-link">Ecommerce</a></li>
+
+                                <li class="footer-menu-item"><a href="#" class="footer-menu-link">Android App</a></li>
+                            </ul>
+
+                            <strong class="site-footer-title d-block mt-4 mb-3">Stay connected</strong>
+
+                            <ul class="social-icon">
+
+                                <li class="social-icon-item"><a href="https://www.instagram.com/fazlul_rmubbaraq/?next=%2F&hl=id" class="social-icon-link bi-instagram"></a></li>
+
+                                <li class="social-icon-item"><a href="https://www.youtube.com/channel/UC1tUn249YDh5YHeAAUWZOtQ" class="social-icon-link bi-youtube"></a></li>
+                                <li class="social-icon-item"><a href="https://api.whatsapp.com/send?phone=6282296539096" class="social-icon-link bi-whatsapp"></a></li>
+                            </ul>
+
+                            <strong class="site-footer-title d-block mt-4 mb-3">Start a project</strong>
+
+                            <p class="mb-0">Saya Bersedia Menjadi Freelance</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-12 ps-lg-0">
+                        <div class="contact-info d-flex flex-column">
+                            <strong class="site-footer-title d-block mb-3">About</strong>
+
+                            <p class="mb-2">
+                                Fazlul merupakan salah satu orang yang merancang website
+                            </p>
+
+                            <strong class="site-footer-title d-block mt-4 mb-3">Email</strong>
+
+                            <p>
+                                <a href="mailto:fazlulrachmat18@gmail.com">
+                                    fazlulrachmat18@gmail.com
+                                </a>
+                            </p>
+
+                            <strong class="site-footer-title d-block mt-4 mb-3">Call</strong>
+
+                            <p class="mb-0">
+                                <a href="tel: 082296539096">
+                                    082296539096
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+
+
+                </div>
+                </form>
+            </div>
+
+            </div>
+            </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12 col-12">
+                    <div class="copyright-text-wrap">
+                        <p class="mb-0">
+                            <span class="copyright-text">Copyright © Fazlul <a href="#">First Portfolio</a> Company. All rights reserved.</span>
+                            Design:
+                            <a rel="sponsored" href="https://templatemo.com" target="_blank">TemplateMo</a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/click-scroll.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
